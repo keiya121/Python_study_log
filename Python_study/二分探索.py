8a45c1e781ed2f56564414c5440b8c731d01f504
KeiyaNimura <107525125+KeiyaNimura@users.noreply.github.com>
2022-06-26 17:53:32 +0900

    二分探索

--- Python_study/二分探索.py
+++ Python_study/二分探索.py
@@ -1,3 +1,6 @@
+# https://atcoder.jp/contests/abc146/tasks/abc146_c 問題
+
+
 A,B,N = map(int,input().split())
 
 def result(x):
@@ -7,7 +10,7 @@ high = 10**9
 low = 0
 mid = (high + low) //2
 
-while 1 < high - low:
+while 1 < high - low:  # highとlow の差分が1より大きい間　境目を見つける。
     mid_price = result(str(mid))
     if mid_price <= N:
         low = mid
