d31eac560755d92d1618f4120012b87c7ae20ed7
KeiyaNimura <107525125+KeiyaNimura@users.noreply.github.com>
2022-07-02 23:50:44 +0900

    ABC258

---
+++ ABC258/258_A.py
@@ -0,0 +1,18 @@
+K = int(input())
+A = ["21","00"]
+
+A[1] = int(A[1]) + K
+
+if int(A[1]) >= 60:
+    A[1] = K%60
+    A[0] = int(A[0]) + K//60
+if 0 <= int(A[1]) < 10:
+        A[1] = str(A[1]).zfill(2)
+
+
+def str_return(x):
+    return str(x)
+
+B = list(map(str_return,A))
+
+print(":".join(B))
